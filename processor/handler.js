65d20be12ec9e7c5dff44764c68f240b754054b2
habboush10 <habboush101@gmail.com>
2018-09-04 18:54:38 +0300

    handler

--- processor/handler.js
+++ processor/handler.js
@@ -3,11 +3,54 @@
 const { TransactionHandler } = require('sawtooth-sdk/processor/handler');
 const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
 const { decode } = require('./services/encoding');
+const { createHash, randomBytes } = require('crypto');
+
+const { getCollectionAddress } = require('./services/addressing');
+const { encode } = require('./services/encoding');
 
 
 const FAMILY_NAME = 'cryptomoji';
 const FAMILY_VERSION = '0.1';
 const NAMESPACE = '5f4d76';
+
+
+const createOwner = (context, { name }, publicKey) => {
+  const address = getCollectionAddress(publicKey);
+  return context.getState([ address ]).then(state => {
+    if (state[address].length > 0) {
+      throw new InvalidTransaction('Owner already exists');
+    }
+    const update = {};
+    update[address] = encode({ key: publicKey, moji: [] });// name is CREATE_OWNER
+    return context.setState(update);
+  });
+}
+
+const _unpackTransaction = (transaction) =>
+  new Promise((resolve, reject) => {
+    let header = TransactionHeader.decode(transaction.header)
+    let signer = header.signerPublicKey
+    try {
+      let payload = _decodeData(transaction.payload)
+      resolve({gameName: payload[0],
+              action: payload[1],
+              space: payload[2],
+              signer: signer})
+    } catch (err) {
+      let reason =  new InvalidTransaction("Invalid payload serialization")
+      reject(reason)
+    }
+  })
+
+
+
+const _makeCollectionAddress = (collectionName) => {
+  console.log(collectionName);
+   let prefix = NAMESPACE;
+   let collectionHash = createHash('sha512').update(collectionName._privateKey).digest('hex').toLowerCase()
+   return prefix + collectionHash.substring(0, 62)
+}
+
 
 /**
  * A Cryptomoji specific version of a Hyperledger Sawtooth Transaction Handler.
@@ -23,7 +66,7 @@ class MojiHandler extends TransactionHandler {
     super(FAMILY_NAME, [ FAMILY_VERSION ], [ NAMESPACE ]);
   }
 
-  /**
+    /**
    * The apply method is where the vast majority of all the work of a
    * transaction processor happens. It will be called once for every
    * transaction, passing two objects: a transaction process request ("txn" for
@@ -46,10 +89,68 @@ class MojiHandler extends TransactionHandler {
    *   - context.deleteState(addresses): deletes the state for the passed
    *     array of state addresses. Only needed if attempting the extra credit.
    */
+
   apply (txn, context) {
     // Enter your solution here
     // (start by decoding your payload and checking which action it has)
+    
 
+    // console.log('*********--------------------****************');
+    // console.log(_unpackTransaction(txn))
+
+
+
+
+    // console.log('-----');
+    // console.log(decode(txn.payload[1]));
+    // console.log('-----');
+    
+
+    //let address = _makeCollectionAddress();
+    
+    let payload = null;
+    try {
+      payload = decode(txn.payload);
+    } catch (err) {
+      throw new InvalidTransaction('Unable to decode payload');
+    }
+
+    // if(payload.action === 'BAD'){
+    //   //context._state[payload];
+    //   throw new InvalidTransaction('Unable to decode payload');
+
+    // }
+    // // else if (payload.action === 'CREATE_COLLECTION') {
+    //   context._state[payload];
+    //   // return 
+    //   // console.log('--------');
+    //   // return console.log(context._state[]);
+    //   // console.log('--------');
+    //   // context.setState(payload._privateKey);
+
+    //   //return createOwner(context, payload, txn.header.signerPublicKey);
+    // } else 
+    
+    if(payload.action !== 'CREATE_COLLECTION') {
+      throw new InvalidTransaction('Unknown action');
+    }
+
+
+    if (payload.action) {
+    console.log('!@#$%^');
+
+      // context.setState(payload._privateKey);
+     const collectionAddress = _makeCollectionAddress(txn)
+
+      const state_entries = context.getState([collectionAddress])
+      return createOwner(context, payload, txn.header.signerPublicKey);
+    } else {
+      context._state[payload];
+    }
+
+
+
+    
   }
 }
 
